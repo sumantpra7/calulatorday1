e4e586f2243a963082762957fd73a58a528aea82
sumant <sumantpra7@gmail.com>
2025-02-07 19:32:41 +0530

    update 1.0

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ function clearDisplay(){
 function deleteLast(){
     display.value = display.value.slice(0,-1);
 }
+function square(){
+    display.value=Math.pow(parseFloat(display.value),2);
+}
+function squareRoot(){
+    display.value=Math.sqrt(parseFloat(display.value))
+}
 function calculateResult(){
 
     try{
